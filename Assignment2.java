50d65e909758b119ba8878c0ebe4d65b74c0726f
Harshit Raj <56254288+harshitraj12@users.noreply.github.com>
2021-08-11 17:47:15 +0530

    DAY 2 Assignment (11.08.2021)

---
+++ Assignment2.java
@@ -0,0 +1,13 @@
+class Assignment2
+{
+      public static void main(String ar[])
+      {
+            int x,y,sum;
+ 
+            x=Integer.parseInt(ar[0]);
+            y=Integer.parseInt(ar[1]);
+ 
+            sum=x+y;
+            System.out.println("sum of " + x + " and " + y +" is " +sum);
+      }
+}
